80e576045faf7e38e15dbf0cc334cca653e8a51c
Lin Lin Hlaing <linlin.hlaing@miu.edu>
2023-06-02 14:47:42 -0500

    intialize the project

---
+++ src/test/java/edu/miu/cs5/onlineShop/OnlineShopApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.miu.cs5.onlineShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
