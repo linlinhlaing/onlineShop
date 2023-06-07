beca5362a04cb929e526dc2ef699ea0d32f61f07
Lin Lin Hlaing <linlin.hlaing@miu.edu>
2023-06-07 14:37:17 -0500

    added domain

---
+++ src/main/java/domain/Address.java
@@ -0,0 +1,9 @@
+package domain;
+
+public class Address {
+    private int addressId;
+    private String street;
+    private String city;
+    private String state;
+    private String zipCode;
+}
---
+++ src/main/java/domain/CreditCard.java
@@ -0,0 +1,10 @@
+package domain;
+
+import java.time.LocalDate;
+
+public class CreditCard {
+    private int creditCardId;
+    private String number;
+    private LocalDate exipreDate;
+    private LocalDate securityCode;
+}
