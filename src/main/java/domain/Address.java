8f576ff6a6da30e737b6af40a79160c311c33750
Lin Lin Hlaing <linlin.hlaing@miu.edu>
2023-06-13 15:22:32 -0500

    added enhance sequence diagram

--- src/main/java/domain/Address.java
+++
@@ -1,9 +0,0 @@
-package domain;
-
-public class Address {
-    private int addressId;
-    private String street;
-    private String city;
-    private String state;
-    private String zipCode;
-}
